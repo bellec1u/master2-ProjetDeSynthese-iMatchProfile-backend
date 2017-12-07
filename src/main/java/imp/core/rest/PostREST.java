ca2077b56bc8477803509d468de10246cb416968
bellec1u <leopold.bellec@gmail.com>
2017-12-07 22:11:45 +0100

    add CUD for posts

--- src/main/java/imp/core/rest/PostREST.java
+++ src/main/java/imp/core/rest/PostREST.java
@@ -5,10 +5,19 @@
  */
 package imp.core.rest;
 
-import imp.core.bean.ExempleRepository;
+import imp.core.bean.PostRepository;
+import imp.core.entity.Exemple;
+import imp.core.entity.Post;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -20,6 +29,28 @@ import javax.ws.rs.Path;
 public class PostREST {
     
     @EJB
-    private ExempleRepository exempleRepo;
+    private PostRepository postRepo;
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response create(Post entity) {
+        Post post = postRepo.create(entity);
+        return Response.ok(post).status(Response.Status.CREATED).build();
+    }
+    
+    @PUT
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response edit(@PathParam("id") Long id, Post entity) {
+        postRepo.edit(entity);
+        return Response.ok().build();
+    }
+    
+    @DELETE
+    @Path("{id}")
+    public Response remove(@PathParam("id") Long id) {
+        postRepo.removeById(id);
+        return Response.ok().build();
+    }
     
 }
