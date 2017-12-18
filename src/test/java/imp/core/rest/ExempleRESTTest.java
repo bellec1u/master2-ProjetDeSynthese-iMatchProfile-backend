4e4192e366b614b812716a3c43b8b745354269fb
bellec1u <leopold.bellec@gmail.com>
2017-12-18 09:56:17 +0100

    [ADD] rest assured

---
+++ src/test/java/imp/core/rest/ExempleRESTTest.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imp.core.rest;
+
+import io.restassured.RestAssured;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+/**
+ *
+ * @author Leopold
+ */
+public class ExempleRESTTest {
+
+    public ExempleRESTTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+        String port = System.getProperty("server.port");
+        if (port == null) {
+            RestAssured.port = Integer.valueOf(8080);
+        } else {
+            RestAssured.port = Integer.valueOf(port);
+        }
+
+        String basePath = System.getProperty("server.base");
+        if (basePath == null) {
+            basePath = "/imp/api/";
+        }
+        RestAssured.basePath = basePath;
+
+        String baseHost = System.getProperty("server.host");
+        if (baseHost == null) {
+            baseHost = "http://localhost";
+        }
+        RestAssured.baseURI = baseHost;
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void getOne() {
+        get("/exemples").then().statusCode(200);
+    }
+}
