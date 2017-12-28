5b6ec52be48be97442261344e54dc0e4ea2429ff
dyasar <yasarfb66@hotmail.fr>
2017-12-28 17:24:54 +0100

    getallpost and getonepost test

--- src/test/java/imp/core/rest/PostRESTTest.java
+++ src/test/java/imp/core/rest/PostRESTTest.java
@@ -43,7 +43,7 @@ public class PostRESTTest {
     @After
     public void tearDown() {
     }
-
+    
     @Test
     public void addPost() {
         JSONObject json = new JSONObject();
@@ -210,6 +210,101 @@ public class PostRESTTest {
 
         given().contentType(MediaType.APPLICATION_JSON)
                 .when().delete("http://localhost:8080/imp/api/posts/" + (int)id)
+                .then().statusCode(200);
+    }
+    
+    
+    @Test 
+     public void getAllPost() {
+        
+        JSONObject json = new JSONObject();
+        json.put("contractType", "test contractType");
+        json.put("description", "test description");
+        json.put("experience", "test experience");
+        json.put("importantNotes", "test importantNotes");
+        json.put("maxSalary", 3);
+        json.put("minSalary", 2);
+        json.put("organization", "test organization");
+
+        JSONArray skills = new JSONArray();
+        JSONObject s1 = new JSONObject();
+        JSONObject java = new JSONObject();
+        java.put("description", "Java");
+        java.put("id", 2);
+        s1.put("skill", java);
+        s1.put("type", "OBLIGATOIRE");
+        skills.add(s1);
+        JSONObject s2 = new JSONObject();
+        JSONObject angular = new JSONObject();
+        angular.put("description", "Angular");
+        angular.put("id", 1);
+        s2.put("skill", angular);
+        s2.put("type", "OBLIGATOIRE");
+        skills.add(s2);
+        json.put("postskill", skills);
+
+        json.put("reference", "test reference");
+        json.put("salaryIndex", "test salaryIndex");
+        json.put("title", "test title");
+        json.put("workUnit", "test workUnit");
+        json.put("workplace", "test workplace");
+
+        JSONObject newJson = 
+                given().contentType(MediaType.APPLICATION_JSON).body(json)
+                .when().post("http://localhost:8080/imp/api/recruiters/13/post")
+                        .body().as(JSONObject.class);
+        
+         given().contentType(MediaType.APPLICATION_JSON)
+                .when().get("http://localhost:8080/imp/api/posts")
+                .then().statusCode(200);
+        
+    }
+     
+     @Test 
+     public void getOnePost() {
+        
+        JSONObject json = new JSONObject();
+        json.put("contractType", "test contractType");
+        json.put("description", "test description");
+        json.put("experience", "test experience");
+        json.put("importantNotes", "test importantNotes");
+        json.put("maxSalary", 3);
+        json.put("minSalary", 2);
+        json.put("organization", "test organization");
+
+        JSONArray skills = new JSONArray();
+        JSONObject s1 = new JSONObject();
+        JSONObject java = new JSONObject();
+        java.put("description", "Java");
+        java.put("id", 2);
+        s1.put("skill", java);
+        s1.put("type", "OBLIGATOIRE");
+        skills.add(s1);
+        JSONObject s2 = new JSONObject();
+        JSONObject angular = new JSONObject();
+        angular.put("description", "Angular");
+        angular.put("id", 1);
+        s2.put("skill", angular);
+        s2.put("type", "OBLIGATOIRE");
+        skills.add(s2);
+        json.put("postskill", skills);
+
+        json.put("reference", "test reference");
+        json.put("salaryIndex", "test salaryIndex");
+        json.put("title", "test title");
+        json.put("workUnit", "test workUnit");
+        json.put("workplace", "test workplace");
+
+        JSONObject newJson = 
+                given().contentType(MediaType.APPLICATION_JSON).body(json)
+                .when().post("http://localhost:8080/imp/api/recruiters/13/post")
+                        .body().as(JSONObject.class);
+        
+        // get id value 
+        double id = (double) newJson.get("id");
+
+        given().contentType(MediaType.APPLICATION_JSON)
+                .when().get("http://localhost:8080/imp/api/posts/" + (int)id)
                 .then().statusCode(200);
     }
     
